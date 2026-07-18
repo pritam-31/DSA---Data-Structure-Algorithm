8a61fc439948d18edbf52c0442f1a581e012b4a6
Pritam Padhan <pritampadhan3107@.com>
2026-07-18 20:05:47 +0530

    Qn-3

--- C++(2026)/Conditional_Statements/Assignment_Qn/Assignment_Qn.cpp
+++ C++(2026)/Conditional_Statements/Assignment_Qn/Assignment_Qn.cpp
@@ -27,13 +27,6 @@ int main() {
         cout << year << "<- It's not a Leap Year" << endl;
     }
 
-    //Qn:-3] What will be the value of x & y in the following program:
-    int a = 63, b = 36;
 
-    bool x = (a < b) ? true : false;
-    int y = (a > b) ? a : b;
-
-    cout << x << "," << y << endl;
-    
     return 0;
 }
