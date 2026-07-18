7c9a9c4a80d60dbb8e0a6052a9498d1b8df39bd4
Pritam Padhan <pritampadhan3107@.com>
2026-07-18 09:03:31 +0530

    assignment questions

---
+++ C++(2026)/Conditional_Statements/Assignment_Qn/Assignment_Qn.cpp
@@ -0,0 +1,31 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+
+    // Write a C++ program to get a number from the user and print whether it's positive, negative or zero.
+    int num;
+    cout << "Enter Number: ";
+    cin >> num;
+
+    if(num > 0) {
+        cout << "It's a +ve number" << endl;
+    } else if(num < 0) {
+        cout << "It's a -ve number" << endl;
+    } else {
+        cout << "You entered zero" << endl;
+    }
+
+    //Write a C++ program that takes a year from the user and print whether that year is a leap year or not.
+    int year;
+    cout << "Enter the Year: ";
+    cin >> year;
+
+    if((year % 4 == 0) && (year % 400 == 0)) {
+        cout << year << "<- It's a Leap Year" << endl;
+    } else {
+        cout << year << "<- It's not a Leap Year" << endl;
+    }
+
+    return 0;
+}
