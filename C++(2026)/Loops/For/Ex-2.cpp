da009591327dcf82c3a4758cb6b836edd7711682
Pritam Padhan <pritampadhan3107@gmail.com>
2026-07-19 21:58:52 +0530

    Ex-2

---
+++ C++(2026)/Loops/For/Ex-2.cpp
@@ -0,0 +1,17 @@
+// Print numbers 1 to n.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, n;
+
+    cout << "Enter n = ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++) {
+        cout << i << endl;
+    }  
+    
+    return 0;
+}
