9829b83ce812da66a6466ae59bfcee8366c4dc3c
Pritam Padhan <pritampadhan3107@gmail.com>
2026-07-22 22:57:31 +0530

    Fibonacci Series

---
+++ C++(2026)/Loops/Assignment_Qns/Qn-6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, first, sec, third;
+    cout << "Enter N: ";
+    cin >> N;
+    
+    first = 0;
+    sec = 1;
+
+    for(int i = 2; i < N; i++) {
+        third = first + sec;
+
+        cout << third << endl;
+
+        first = sec;
+        sec = third;
+    } 
+
+    return 0;
+}
