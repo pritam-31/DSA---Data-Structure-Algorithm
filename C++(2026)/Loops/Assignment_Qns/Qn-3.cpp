2e70885f76d208232535d63d1a8a8643f1dce2bb
Pritam Padhan <pritampadhan3107@gmail.com>
2026-07-23 22:58:25 +0530

    new logic for Check Armstrong no.

--- C++(2026)/Loops/Assignment_Qns/Qn-3.cpp
+++ C++(2026)/Loops/Assignment_Qns/Qn-3.cpp
@@ -22,3 +22,36 @@ int main() {
 
     return 0;
 }
+
+or
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, num, digit, count = 0;
+    int sum = 0;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    //count digits
+    for(num = n; num > 0; num /= 10) {
+        count++;
+    }
+
+    //calculate sum of digit raised to count
+    for (num = n; num > 0; num /= 10) {
+        digit = num % 10;
+        sum = sum + pow(digit, count);
+    }
+
+    if(sum == num) {
+        cout << n << " is an Armstrong no.";
+    } else {
+        cout << n << " is not an Armstrong no.";
+    }
+
+    return 0;
+}
