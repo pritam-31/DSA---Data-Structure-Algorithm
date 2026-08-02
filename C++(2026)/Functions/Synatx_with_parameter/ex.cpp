c0456132234e7c63d51f63ad6258b5a6a0fbfb58
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-02 08:07:53 +0530

    .

--- C++(2026)/Functions/Synatx_with_parameter/ex.cpp
+++ C++(2026)/Functions/Synatx_with_parameter/ex.cpp
@@ -5,8 +5,14 @@ void myFunc() {
     cout << "Hello \n";
 }
 
+void multiply(int a, int b) {
+    int multi = a * b;
+    cout << multi;
+}
+
 int main() {
     myFunc();
+    multiply(1, 6);
 
     return 0;
 }
