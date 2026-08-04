4d56061d6c038264658804a8b04cec06e977cfbf
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-04 18:50:25 +0530

    Print all Primes in a Range from (2 to n)

---
+++ C++(2026)/Functions/Practice_qn/qn_5.cpp
@@ -0,0 +1,34 @@
+// Print all Primes in a Range from (2 to n)
+
+#include <iostream> 
+using namespace std;
+
+bool isPrime(int n) {
+    if(n == 1) {
+        return false;
+    }
+
+    for(int i=2; (i*i)<=n; i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void allPrimes(int n) {
+    for(int i=2; i<=n; i++) {
+        if(isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+
+    allPrimes(12);
+
+    return 0;
+}
