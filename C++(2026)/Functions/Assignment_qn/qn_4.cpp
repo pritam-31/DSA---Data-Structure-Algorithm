20ac61fb4052b030563a034e3d065e59e4050cba
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-08 09:54:17 +0530

    Write a function that prints the largest of 3 numbers.

---
+++ C++(2026)/Functions/Assignment_qn/qn_4.cpp
@@ -0,0 +1,25 @@
+// Write a function that prints the largest of 3 numbers.
+
+#include <iostream>
+using namespace std;
+
+int largeNo(int a, int b, int c) {
+    if( ( a>=b ) && (a >= c) ) {
+        return a;
+    } else if(b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
+int main() {
+
+    int a, b, c;
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+
+    cout << "Largest no. is: " << largeNo(a, b, c) << endl;
+
+    return 0;
+}
