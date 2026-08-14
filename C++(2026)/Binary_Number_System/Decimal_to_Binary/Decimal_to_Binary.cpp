ba0e0e6fb35857eef9f398190e2488bb743ee2cf
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-14 14:56:27 +0530

    for decimal to binary conversion

---
+++ C++(2026)/Binary_Number_System/Decimal_to_Binary/Decimal_to_Binary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void binary(int n)
+{
+    if (n == 0) {
+        return;
+    }
+        
+    binary(n / 2);
+    cout << n % 2;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    binary(n);
+
+    return 0;
+}
