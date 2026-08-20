766194b2d88e058bba5a54e76586d1e4ac6b2494
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-20 20:42:21 +0530

    Add initial implementation of array declaration example in C++

---
+++ C++(2026)/Array_1/declaration_of_array/ex_1.cpp
@@ -0,0 +1,16 @@
+// declaration of array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int marks[] = {1, 2, 3, 4};
+
+cout << marks[0] << endl; // 1
+cout << marks[1] << endl; // 2
+cout << marks[2] << endl; // 3
+cout << marks[3] << endl; // 4
+
+return 0;
+}
