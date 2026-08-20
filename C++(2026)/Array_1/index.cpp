6d7febfbecaed0ca49f567a8fd64757b2bdb16ca
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-20 20:32:53 +0530

    Initialized an array of integers with a size of 50. - Output the first four elements of the array and an out-of-bounds element for demonstration.

---
+++ C++(2026)/Array_1/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int marks[50];
+
+cout << marks[0] << endl;
+cout << marks[1] << endl;
+cout << marks[2] << endl;
+cout << marks[3] << endl;
+cout << marks[50] << endl;
+
+return 0;
+}
