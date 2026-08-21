50b4e040ae66f758a5daeb0708213b7b5844e2f0
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-21 20:27:00 +0530

    Add implementation for input and output of an array in C++

---
+++ C++(2026)/Array_1/inp_of_array/ex_2.cpp
@@ -0,0 +1,30 @@
+// Input of the Array way 2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    int nn = sizeof(arr) / sizeof(int); // Length/ Size of Array
+
+    for (int i=0; i < nn; i++) {
+        cin >> arr[i]; // Input of Array
+    }
+
+    for (int i=0; i < nn; i++) {
+        cout << arr[i] << ", "; // Output of Array
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+// o/p- 
+// 1
+// 5
+// 8
+// 6
+// 7
+
+//--> 1, 5, 8, 6, 7,
