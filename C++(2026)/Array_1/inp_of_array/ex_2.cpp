0f0dcf68e7739e54a8d1fc3297e3648441a3d785
Pritam <pritampadhan3107@gmail.com>
2026-08-22 11:31:25 +0530

    Update ex_2.cpp

--- C++(2026)/Array_1/inp_of_array/ex_2.cpp
+++ C++(2026)/Array_1/inp_of_array/ex_2.cpp
@@ -11,7 +11,7 @@ int main() {
         cin >> arr[i]; // Input of Array
     }
 
-    for (int i=0; i < nn; i++) {
+    for (int i=0; i < n; i++) {
         cout << arr[i] << ", "; // Output of Array
     }
     cout << endl;
