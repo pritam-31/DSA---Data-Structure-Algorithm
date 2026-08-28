1ef2a43a8e050f4b376129c3b49e11ab0dca2921
Pritam Padhan <pritampadhan3107@gmail.com>
2026-08-28 21:44:39 +0530

    Refactor printArray function and update main for clarity

--- C++(2026)/c.cpp
+++ C++(2026)/c.cpp
@@ -3,8 +3,21 @@
 #include <iostream> 
 using namespace std;
 
+void printArray(int nums[], int n) {   // yeh sahi hai, (int n -> size of array)
+
+    // int n = sizeof(nums) / sizeof(nums[0]); yeh galat hai
+
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, n);
 
     return 0;
 }
