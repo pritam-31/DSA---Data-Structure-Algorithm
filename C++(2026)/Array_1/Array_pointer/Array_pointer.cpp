c9436158c91b031817b2fc9913013a41badeb343
Pritam Padhan <pritampadhan3107@gmail.com>
2026-09-02 20:45:00 +0530

    Fix output statement for array pointer display

--- C++(2026)/Array_1/Array_pointer/Array_pointer.cpp
+++ C++(2026)/Array_1/Array_pointer/Array_pointer.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     int arr[5];
-    cout << arr < "\n";
+    cout << arr << "\n";
 
     int y = 25;
     arr = &y; // This will give an error because arr is a constant pointer and cannot be assigned to point to another location in memory.
