4578e1b98aed482698b4129366f0e9e1ae9da006
Pritam Padhan <pritampadhan3107@gmail.com>
2026-09-03 20:14:26 +0530

    addition & substraction

---
+++ C++(2026)/Array_1/Arithmetic_Pointer/Arithmetic_Pointer3.cpp
@@ -0,0 +1,20 @@
+// addition & subtraction of pointer and array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int a = 5;
+
+    // in Array->
+    int arr[20] = {1, 2, 3, 4, 5, 6};
+    int *ptr1 = arr;
+    int *ptr2 = ptr1 + 3;
+
+    cout << *ptr1 << "\n"; // 1
+    cout << *ptr2 << "\n"; // 4
+
+    cout << (ptr2 - ptr1) << endl; // 3
+
+    return 0;
+}
