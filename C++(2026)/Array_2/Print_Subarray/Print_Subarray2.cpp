5fa496e2fa88fc5ae6468a3c1bd1bcdf99aee023
Pritam Padhan <pritampadhan3107@gmail.com>
2026-09-05 21:06:05 +0530

    Refactor printSubarrays function output format and add HTML representation of subarrays

--- C++(2026)/Array_2/Print_Subarray/Print_Subarray2.cpp
+++ C++(2026)/Array_2/Print_Subarray/Print_Subarray2.cpp
@@ -6,11 +6,11 @@ void printSubarrays(int *arr, int sz) {
         for (int end = start; end < sz; end++) {
 
             for (int i = start; i <= end; i++) {
-                cout << arr[i] << " ";
+                cout << arr[i];
             }
-
-            cout << endl;   // har subarray ke baad new line
+            cout << ", ";
         }
+        cout << endl;
     }
 }
 
@@ -22,23 +22,3 @@ int main() {
 
     return 0;
 }
-
-Output:
-
-1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
-2
-2 3
-2 3 4
-2 3 4 5
-3
-3 4
-3 4 5
-4
-4 5
-5
-
-Ek aur chhota correction: tumne function ko "int" banaya tha, lekin function kuch return nahi kar raha. Isliye maine "int" ki jagah "void" kar diya.
