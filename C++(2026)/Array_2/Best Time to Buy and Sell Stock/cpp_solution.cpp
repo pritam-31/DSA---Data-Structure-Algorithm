c08166967174be74870b5e9dec9dc8f04c4583ad
Pritam Padhan <pritampadhan3107@gmail.com>
2026-09-24 20:42:38 +0530

    final solutions

--- C++(2026)/Array_2/Best Time to Buy and Sell Stock/cpp_solution.cpp
+++ C++(2026)/Array_2/Best Time to Buy and Sell Stock/cpp_solution.cpp
@@ -8,15 +8,26 @@ int maxProfit(int *prices, int n) {
     bestBuy[0] = INT_MAX;
     cout << bestBuy[0] << ", ";
 
+    // 1st step: har ek sell-price ke liye, hum uske "bestBuy-price calculated karege"
     for(int i=1; i<n; i++) {
         bestBuy[i] = min(bestBuy[i-1], prices[i-1]);
         cout << bestBuy[i] << ", ";
     }
     cout << endl;
+
+    // 2nd step: har kisi "i" ke liye, uska profit nikalenge..(bestBuy ke hisab se profit calculate karne ke)
+    int maxProfit = 0;
+
+    for(int i=0; i< n; i++) {
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(maxProfit, currProfit);
+    }
+
+    cout << "Max Profit = " << maxProfit << endl;
 }
 
 int main() {
-    int prices[6] = {7, 1, 5, 3, 6, 4};
+    int prices[6] = {7, 1, 5, 3, 6, 4};  //if prices decrease hote jayega toh profit = 0 hoga
     int n = sizeof(prices) / sizeof(int);
 
     maxProfit(prices, n);
